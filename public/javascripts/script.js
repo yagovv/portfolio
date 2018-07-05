52e7b7dfb1a289a6931e2226c669d99650c2081b
Yago Vega <yagofvv@gmail.com>
2018-07-05 14:37:42 -0400

    added button functionality

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('IronGenerator JS imported successfully!');
-
+  let flow = document.getElementById('sub-flow');
+  const web = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
+      <div class="card">
+        <img src="images/ironhackers.png" alt="image">
+        <div class="sub-card">Ironhackers of the Caribbean</div>
+      </div>
+    </a>
+    <a href="https://planit-ironhack.herokuapp.com/">
+      <div class="card">
+        <img src="images/planit.png" alt="image">
+        <div class="sub-card">PLANiT</div>
+      </div>
+    </a>
+    <a href="https://zombye.herokuapp.com/">
+      <div class="card">
+        <img src="images/zombye.png" alt="image">
+        <div class="sub-card">Zombye</div>
+      </div>
+    </a>`;
+  const ux = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
+      <div class="card">
+        <img src="images/ironhackers.png" alt="image">
+        <div class="sub-card">Wellness Project</div>
+      </div>
+    </a>
+    <a href="https://planit-ironhack.herokuapp.com/">
+      <div class="card">
+        <img src="images/planit.png" alt="image">
+        <div class="sub-card">e-Groceries Project</div>
+      </div>
+    </a>
+    <a href="https://zombye.herokuapp.com/">
+      <div class="card">
+        <img src="images/zombye.png" alt="image">
+        <div class="sub-card">Final Project</div>
+      </div>
+    </a>`;
+    let webButton = document.getElementById('web-btn');
+    let uxButton = document.getElementById('ux-btn');
+    webButton.addEventListener('click', ()=>{
+        flow.innerHTML = web;
+    });
+    uxButton.addEventListener('click', ()=>{
+        flow.innerHTML = ux;
+    });
 }, false);

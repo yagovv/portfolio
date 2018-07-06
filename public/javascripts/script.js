2c881697b8d73547e439da374631f0eb7e92164b
Yago Vega <yagofvv@gmail.com>
2018-07-06 09:42:10 -0400

    new version

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let flow = document.getElementById('sub-flow');
-  const web = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    let title = document.getElementById("main-title");
+    new TypeIt(title, {
+      strings: ["Welcome.", "I'm Yago Vega, and this is my portfolio."],
+      speed: 125,
+      breakLines: false,
+      autoStart: false
+    });
+    let flow = document.getElementById("main-flow");
+    setTimeout(() => {
+      title.setAttribute("class", "upper");
+      setTimeout(() => {
+        flow.innerHTML = `
+    <div id="upper-div" class="main-btn">
+    <h1>About Me</h1>
+  </div>
+  <div id="lower-div" class="main-btn">
+    <h1>My Projects</h1>
+  </div>
+    `;
+      }, 2000);
+    }, 8000);
+
+    const web = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
       <div class="card">
         <img src="images/ironhackers.png" alt="image">
         <div class="sub-card">Ironhackers of the Caribbean</div>
@@ -18,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="sub-card">Zombye</div>
       </div>
     </a>`;
-  const ux = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
+    const ux = `<a href="https://yagovv.github.io/Ironhackers-of-the-Caribbean/">
       <div class="card">
         <img src="images/ironhackers.png" alt="image">
         <div class="sub-card">Wellness Project</div>
@@ -36,12 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="sub-card">Final Project</div>
       </div>
     </a>`;
-    let webButton = document.getElementById('web-btn');
-    let uxButton = document.getElementById('ux-btn');
-    webButton.addEventListener('click', ()=>{
-        flow.innerHTML = web;
-    });
-    uxButton.addEventListener('click', ()=>{
-        flow.innerHTML = ux;
-    });
-}, false);
+    // let webButton = document.getElementById("web-btn");
+    // let uxButton = document.getElementById("ux-btn");
+    // webButton.addEventListener("click", () => {
+    //   flow.innerHTML = web;
+    // });
+    // uxButton.addEventListener("click", () => {
+    //   flow.innerHTML = ux;
+    // });
+  },
+  false
+);
---
+++ views/components/test.js
@@ -0,0 +1,5 @@
+export default `
+<div>
+  <h1 class="test">Testerino pan y vino</h1>
+</div>
+`;
